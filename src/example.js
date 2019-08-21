337345dde084996e2b807e854a8b6657585e257f
SHCBuildUser <milo@DuyNguyens-MacBook-Pro-2.local>
2019-08-21 12:11:15 +0700

    test code coverage

---
+++ src/example.js
@@ -0,0 +1,9 @@
+function sayHello() {
+    return "Hello Siouxers"
+}
+
+function sayGoodbye() {
+    return "Bye bye Siouxers"
+}
+
+module.exports = { sayHello, sayGoodbye};
--- tests/example.test.js
+++ tests/example.test.js
@@ -1,3 +1,17 @@
+const { sayHello, sayGoodbye } = require('../src/example')
+
+describe('say hello', () => {
+    it('should say hello to Siouxers', () => {
+        expect(sayHello()).toBe('Hello Siouxers');
+    });
+});
+
+describe('say goodbye', () => {
+    it('should say goodbye to Siouxers', () => {
+        expect(sayGoodbye()).toBe('Bye bye Siouxer');
+    });
+});
+
 describe('addition', () => {
     describe('positive numbers', () => {
       it('should add up', () => {
