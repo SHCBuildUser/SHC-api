bcdda5fcaed0162a0960ec3cb6d745d46115e429
SHCBuildUser <milo@DuyNguyens-MacBook-Pro-2.local>
2019-08-21 15:14:35 +0700

    test for heroku

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -4,11 +4,11 @@ set -eu
 
 git remote add heroku https://git.heroku.com/$HEROKU_APP_NAME.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
-mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
-cat > ~/.netrc << EOF
+sudo cat > ~/.netrc << EOF
 machine api.heroku.com
 	login $HEROKU_LOGIN
 	password $HEROKU_API_KEY
